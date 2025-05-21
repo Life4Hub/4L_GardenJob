f2d92d1dd0a258328d483964631749cb90acdf90
Life4Tune <66623092+Life4Hub@users.noreply.github.com>
2025-05-21 22:26:10 +0200

    Add files via upload

---
+++ 4L_gardener/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'Life4tune'
+description 'Garden Job'
+version '1.1'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/sv_main.lua'
+}
+
+client_script 'client/cl_main.lua'
+
+dependencies {
+    'es_extended',
+    'oxmysql',
+    'ox_lib'
+}
